e6656a2115a658aad972c294b037d6902178e3f4
Robert Meissner <jahrome144000@gmail.com>
2024-03-11 12:24:34 +0100

    fix: store

--- stores/Global.js
+++ stores/Global.js
@@ -1,27 +1,29 @@
 let store = {};
 
-export function useStore(name = "default", init = {_obs: []}) {
+export function useStore(name = "default", _init = {_obs: []}) {
   if (!store[name]) {
-    store[name] = init;
+    store[name] = _init;
   }
   function subscribe(target, context) {
-    if (!store[name][target]) {
-      store[name][target] = { _value: undefined };
-      store[name][target]["_obs"] = [];
+    if (target) {
+      if (!store[name][target]) {
+        store[name][target] = { _value: undefined };
+        store[name][target]["_obs"] = [];
+      }
+      store[name][target]["_obs"].push(context);
+    } else {
+      store[name]["_obs"].push(context);
     }
-    store[name][target]["_obs"].push(context);
   }
   function update(target, value) {
     store[name][target]["_value"] = value;
-    var subscribers = [...store[name][target]["_obs"]];
+    var subscribers = [...store[name][target]["_obs"],...store[name]["_obs"]];
     store[name][target]["_obs"] = [];
     for (var i = 0; i < subscribers.length; i++) {
       function getHost(hostDataIDs) {
         let shadowDOM = document;
         for (let hostDataID of hostDataIDs) {
-          const host = shadowDOM.querySelector(
-            '[data-id="' + hostDataID + '"]'
-          );
+          const host = shadowDOM.querySelector('[data-id="' + hostDataID + '"]');
           if (host) {
             shadowDOM = host.shadowRoot;
           } else {
